9432512dd6655f93226e2eeca83b47bb4650b2a2
Frederick Goy IV <fredgoy2003@gmail.com>
2017-11-21 10:51:41 -0500

    make the thing actually work now that i can use openerTabId

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+/*
+	explicitly opening a new tab:
+		the + button  		- new tab opens, onCreated fires
+		ctrl T        		- new tab opens, onCreated fires
+		
+	via links:
+		a target=_blank 	- new tab opens, onCreated fires
+		middle mouse  		- new tab opens, onCreated does not fire
+		context menu		- new tab opens, onCreated does not fire
+		ctrl click			- new tab opens, onCreated does not fire
+		
+	"suprise" new tabs:
+		undo closed tab		- new tab opens, onCreated fires
+*/
+
 function checkTab(tab) {
 	function redirectTab(result) {
 		browser.tabs.update(tab.id, {url: result.url})
 	}
 	
 	function doError(error) {
-		console.error(error);
+		console.log(error);
 	}
 	
 	if (tab.title == "New Tab") {
+		console.log(tab.title + " " + tab.id + " " + tab.status);
+		if (tab.openerTabId) {
+			return;
+		}
 		var getURL = browser.storage.local.get("url");
 		getURL.then(redirectTab, doError);
 	}
